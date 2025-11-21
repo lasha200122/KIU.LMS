d7967a0df72faff9b53adda9daac2e12c68af80f
shaw3rm5 <12345678+shaw3rm5@users.noreply.github.com>
2025-11-21 15:29:23 +0400

    add migrations

---
+++ KIU.LMS.Persistence/Migrations/20251121095412_AddEntitiesForVotes.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KIU.LMS.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEntitiesForVotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VotingSessions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    EndTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreateUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LastUpdateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LastUpdateUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DeleteDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeleteUserId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VotingSessions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Votes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OptionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreateUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LastUpdateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LastUpdateUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DeleteDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeleteUserId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Votes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Votes_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Votes_VotingSessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "VotingSessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VotingOptions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SessionId = table.Column<Guid>(type: "uuid", nullable: false),
+                    FileRecordId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CreateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    CreateUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LastUpdateDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    LastUpdateUserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "boolean", nullable: false),
+                    DeleteDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeleteUserId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VotingOptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VotingOptions_FileRecord_FileRecordId",
+                        column: x => x.FileRecordId,
+                        principalTable: "FileRecord",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_VotingOptions_VotingSessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "VotingSessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Votes_CreateDate",
+                table: "Votes",
+                column: "CreateDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Votes_SessionId_UserId",
+                table: "Votes",
+                columns: new[] { "SessionId", "UserId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Votes_UserId",
+                table: "Votes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VotingOptions_CreateDate",
+                table: "VotingOptions",
+                column: "CreateDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VotingOptions_FileRecordId",
+                table: "VotingOptions",
+                column: "FileRecordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VotingOptions_SessionId_FileRecordId",
+                table: "VotingOptions",
+                columns: new[] { "SessionId", "FileRecordId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VotingSessions_CreateDate",
+                table: "VotingSessions",
+                column: "CreateDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Votes");
+
+            migrationBuilder.DropTable(
+                name: "VotingOptions");
+
+            migrationBuilder.DropTable(
+                name: "VotingSessions");
+        }
+    }
+}
