06420eb34b2167665ac0823764db788dd2d5b1f6
shaw3rm5 <12345678+shaw3rm5@users.noreply.github.com>
2025-11-27 12:34:38 +0400

    update migrations

---
+++ KIU.LMS.Persistence/Migrations/20251127083420_AddForeignKeyForVoting.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KIU.LMS.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForeignKeyForVoting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Votes_OptionId",
+                table: "Votes",
+                column: "OptionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Votes_VotingOptions_OptionId",
+                table: "Votes",
+                column: "OptionId",
+                principalTable: "VotingOptions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Votes_VotingOptions_OptionId",
+                table: "Votes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Votes_OptionId",
+                table: "Votes");
+        }
+    }
+}
